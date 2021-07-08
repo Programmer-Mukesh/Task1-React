ed9ba04424fe948857cc0f8b7bef37e9f94c2f39
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-08 14:45:25 +0530

    Update Form.js

--- src/Formcontainer/Form.js
+++ src/Formcontainer/Form.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react'
-import Input from './Input'
-import "./Form.css"
+import React, { Component } from "react";
+import Input from "./Input";
+import "./Form.css";
 
 class Form extends Component {
-    render(){
-        return (
-            <div>
-                <form className="form" action="">
-                    <Input insideText={"First Name*"}/>
-                    <Input insideText={"Last Name*"}/>
-                    <Input insideText={"City, State and ZIP*"}/>
-                    <Input insideText={"Email*"} type="email"/>
-                    <Input insideText={"Password*(at least 6 characters)"} type="password"/>
-                    <select className="dropdown">
-                        <option value="select">How Did You Hear About Us?</option>
-                        <option value="select">Facebook</option>
-                        <option value="select">Google</option>
-                        <option value="select">Relatives</option>
-                    </select>
-                </form>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      content1: "First Name*",
+      content2: "Last Name*",
+      content3: "City, State and ZIP*",
+      content4: "Email*",
+      content5: "Password*(at least 6 characters)",
+    };
+  }
+
+  render() {
+    const { content1, content2, content3, content4, content5 } = this.state;
+    const options = [
+      "How Did You Hear About Us?",
+      "Facebook",
+      "Google",
+      "Relatives",
+    ];
+
+    return (
+      <div>
+        <form className="form" action="">
+          <Input insideText={content1} />
+          <Input insideText={content2} />
+          <Input insideText={content3} />
+          <Input insideText={content4} type="email" />
+          <Input insideText={content5} type="password" />
+          <select className="dropdown">
+            {options.map((x) => (
+              <option value="select">{x}</option>
+            ))}
+          </select>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default Form
+export default Form;
