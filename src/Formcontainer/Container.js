9e9ad9642820026dec732d0b60769a0811c9863f
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-08 14:42:17 +0530

    Update Container.js

--- src/Formcontainer/Container.js
+++ src/Formcontainer/Container.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react'
-import "./Container.css"
-import Heading from './Heading'
-import SubHeading from './SubHeading';
-import Radio from './Radio';
-import Form from './Form';
-import Footer from './Footer';
+import React, { Component } from "react";
+import "./Container.css";
+import Heading from "./Heading";
+import SubHeading from "./SubHeading";
+import Radio from "./Radio";
+import Form from "./Form";
+import Footer from "./Footer";
 
-class Container extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            text1: "Join free today.",
-            text2: "Get Started! It only takes a few minutes."
-        }
-    }
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text1: "Join free today.",
+      text2: "Get Started! It only takes a few minutes.",
+    };
+  }
 
-    render(){
-        return(
-            <div className="Container">
-                <Heading text={this.state.text1}/>
-                <SubHeading text={this.state.text2}/>
-                <Radio/>
-                <Form/>
-                <Footer/>
-            </div>
-        )
-    }
+  render() {
+    const { text1, text2 } = this.state;
+    return (
+      <div className="Container">
+        <Heading text={text1} />
+        <SubHeading text={text2} />
+        <Radio />
+        <Form />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Container;
