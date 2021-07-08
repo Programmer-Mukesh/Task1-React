4735230e321896a623d1d9aabe9235a1a1cea112
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-08 14:48:38 +0530

    Update Radio.js

--- src/Formcontainer/Radio.js
+++ src/Formcontainer/Radio.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './Radio.css';
+import React from "react";
+import "./Radio.css";
 
-const Radio=() => <div className="Radio">
-                    <p className="question">What would you like to do?</p>
-                    <input className="radio" type="radio" value="Hire a caregiver" /> Hire a caregiver
-                    <input className="radiotxt radio" type="radio" value="Apply for a care jobs"/> Apply for a care jobs             
-                  </div>
-    
-export default Radio
+const Radio = () => (
+  <div className="Radio">
+    <p className="question">What would you like to do?</p>
+    <input className="radio" type="radio" value="Hire a caregiver" /> Hire a
+    caregiver
+    <input
+      className="radiotxt radio"
+      type="radio"
+      value="Apply for a care jobs"
+    />{" "}
+    Apply for a care jobs
+  </div>
+);
+
+export default Radio;
