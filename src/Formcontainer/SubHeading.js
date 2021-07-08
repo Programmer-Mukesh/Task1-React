6350c4bea5b3a9662dad7dea8d1f74c6760879f3
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-08 14:49:42 +0530

    Update SubHeading.js

--- src/Formcontainer/SubHeading.js
+++ src/Formcontainer/SubHeading.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 import "./SubHeading.css";
 
-const SubHeading=({text}) =>{
-    return(
-        <div className="SubHeading">
-            <h3>{text}</h3>
-        </div>
-    )
-}
+const SubHeading = ({ text }) => (
+  <div className="SubHeading">
+    <h3>{text}</h3>
+  </div>
+);
 
-export default SubHeading
+export default SubHeading;
